fb5afb16ae983937fc9ed0b220df82f2fc0e55ed
qweasd <qweasd333ee@gmail.com>
2023-01-04 16:33:30 +0800

    修改輪播圖

--- JS/main.js
+++ JS/main.js
@@ -2,22 +2,21 @@ $('.carousel').slick({
   infinite: true,
   speed: 300,
   slidesToShow: 4,
-  slidesToScroll: 4,
+  slidesToScroll: 1,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+        slidesToScroll: 1,
+        // infinite: true,
       }
     },
     {
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 1
       }
     },
     {
